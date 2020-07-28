05be756d6edeff142db5b0a1fd1843f74b16b468
Shira Amrany <shira.amrany@gmail.com>
2020-07-28 19:03:05 +0000

    pass True to continue with cc func and unpack each operation

--- part2.py
+++ part2.py
@@ -37,87 +37,98 @@ def luhn_check(cc_num):
 
 	else: 
 		print("FALSE")
-		return False
+		return True  #FIX AFTER
 
 		
-# def ccProvider(operations):
+def ccProvider(operations):
 	
-# 	balance_info = {} 
+	balance_info = {} 
 
-# 	for ops in operations:
-# 		if ops[0] == "Add":
-# 			# "Add", "Tom", "4111111111111111", "$1000"
-# 			# if validate cc num passes:
-# 			limit = int(ops[3].split('$')[1])
+	for ops in operations:
+		# if len(ops) == 4:
+		# 	action, person, card_num, limit = ops
+
+		# elif len(ops) == 3:
+		# 	action, person, amount = ops
+
+
+		if ops[0] == "Add":
+			action, person, card_num, limit = ops
+			# if validate cc num passes:
+			limit = int(limit.split('$')[1])
 			
-# 			if luhn_check(ops[2]) == True:	
-# 				balance_info[ops[1]] = {'card_number': ops[2], 'limit': limit, 'balance': 0}
+			if luhn_check(card_num) == True:	
+				balance_info[person] = {'card_number': card_num, 'limit': limit, 'balance': 0}
 
-# 			else:
-# 				balances_info[ops[1]]= {'card_number': 'invalid', 'limit': limit, 'balance': 'error'}
+			else:
+				balance_info[ops[1]]= {'card_number': 'invalid', 'limit': limit, 'balance': 'error'}
+
+			print(balance_info)
+
+		elif ops[0] == "Charge":
+			action, person, amount = ops
+			charge = int(amount.split('$')[1])
+			
+			if balance_info[person]['balance'] + charge > balance_info[person]['limit']: 
+				# if balance_info[:
+				pass
+
+			else:
+				balance_info[person]['balance'] += charge
 
 
-# 		elif ops[0] == "Charge":
-# 			charge = int(ops[2].split('$')[1])
-# 			if balance_info[ops[1]]['balance'] + charge > balance_info[ops[1]]['limit']:
-# 				pass
-
-# 			else:
-# 				balance_info[ops[1]]['balance'] += charge
-
-
-# 		elif ops[0] == "Credit":
-# 			credit = int(ops[2].split('$')[1])
-# 			balance_info[ops[1]]['balance'] -= credit
+		elif ops[0] == "Credit":
+			action, person, amount = ops
+			credit = int(amount.split('$')[1])
+			balance_info[person]['balance'] -= credit
 
 	
-# 	balance_output = []
+	balance_output = []
 
-# 	for person in balance_info:
-# 		balance = balance_info[person].get('balance')
-# 		balance_output.append[person, balance]
-# 		# print(person)
-# 		# print(balance_info[person].get('balance'))
+	for person in balance_info:
+		print(person)
+		print(balance_info[person].get('balance'))
+		# balance = 
+		# balance_output.append[person, balance]
+		# print(person)
+		# print(balance_info[person].get('balance'))
 
-# 	# 	balance_output.append[person, person['balance']]
+	# 	balance_output.append[person, person['balance']]
 
-# 	# print(balance_output)
-
-
-
-# # card_number --> validate w. luhn 0
+	# print(balance_output)
 
 
 
-# # "charge"
-# # against luhn 10 invalid cards are ignored. ??
 
-# # "credit"
-# # credits against luhn 10 cards are ignores
+# "charge"
+# against luhn 10 invalid cards are ignored. ??
+
+# "credit"
+# credits against luhn 10 cards are ignores
 
 
-# # return card holder names with balance associated.
-# # display "error" instead of balance if cc does not
-# # pass Luhn10. Display in lexicographical(alpha) order
+# return card holder names with balance associated.
+# display "error" instead of balance if cc does not
+# pass Luhn10. Display in lexicographical(alpha) order
 
-# # operations = [["Add", "Tom", "4111111111111111", "$1000"],
-# #               ["Add", "Lisa", "5454545454545454", "$3000"],
-# #               ["Add", "Quincy", "1234567890123456", "$2000"],
-# #               ["Charge", "Tom", "$500"],
-# #               ["Charge", "Tom", "$800"],
-# #               ["Charge", "Lisa", "$7"],
-# #               ["Credit", "Lisa", "$100"],
-# #               ["Credit", "Quincy", "$200"]]
-
-# # creditCardProvider(operations) = [["Lisa", "$-93"],
-# #                                   ["Quincy", "error"],
-# #                                   ["Tom", "$500"]]
-
-# ccProvider([["Add", "Tom", "4111111111111111", "$1000"],
+# operations = [["Add", "Tom", "4111111111111111", "$1000"],
 #               ["Add", "Lisa", "5454545454545454", "$3000"],
 #               ["Add", "Quincy", "1234567890123456", "$2000"],
 #               ["Charge", "Tom", "$500"],
 #               ["Charge", "Tom", "$800"],
 #               ["Charge", "Lisa", "$7"],
 #               ["Credit", "Lisa", "$100"],
-#               ["Credit", "Quincy", "$200"]])
+#               ["Credit", "Quincy", "$200"]]
+
+# creditCardProvider(operations) = [["Lisa", "$-93"],
+#                                   ["Quincy", "error"],
+#                                   ["Tom", "$500"]]
+
+ccProvider([["Add", "Tom", "4111111111111111", "$1000"],
+              ["Add", "Lisa", "5454545454545454", "$3000"],
+              ["Add", "Quincy", "1234567890123456", "$2000"],
+              ["Charge", "Tom", "$500"],
+              ["Charge", "Tom", "$800"],
+              ["Charge", "Lisa", "$7"],
+              ["Credit", "Lisa", "$100"],
+              ["Credit", "Quincy", "$200"]])
