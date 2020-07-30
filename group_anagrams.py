3be1311e1e49eab95a29a4fd9c2b30c078506e6b
Shira Amrany <shira.amrany@gmail.com>
2020-07-30 20:24:37 +0000

    new day approach with sorted string as keys

--- group_anagrams.py
+++ group_anagrams.py
@@ -1,41 +1,17 @@
 
 def groupAnagrams(my_list):
-
-# Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
-# Output:
-# [
-#   ["ate","eat","tea"],
-#   ["nat","tan"],
-#   ["bat"]
-# ]
-# {"eat":{'e':1, 'a': 1, 't': 1}, "tea":{}}
    
     output_list = []
-    my_dict = {}
-    value_set = set()
+    word_key_dict = {}
     
     for word in my_list:
-        my_dict[word] = {}
-        
-        for char in word:
-            my_dict[word][char] = my_dict[word].get(char,0) + 1
-            
-        # value_set.add(my_dict[word])
+        sorted_word = "".join(sorted(word))
+        word_key_dict[sorted_word] = word_key_dict.get(sorted_word, []) + [word]
+  
+    for key, value in word_key_dict.items():
+        print(value)
 
-    print(my_dict)
-    print(value_set)
  
 
 groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])       
     
-    # for item in value_set:
-    #     group_list = []
-    #     #get key where value is equal
-
-    #     group_list.append(match)
-    #     output
-    # dict.
-                    
-    # extract the values into a set
-    # loop over set and for each item
-    #     add keys with that value to a list to output
