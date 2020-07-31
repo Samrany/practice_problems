149f46cbda150889a8c9254d159f021a066616ba
Shira Amrany <shira.amrany@gmail.com>
2020-07-31 23:17:15 +0000

    clean up

--- reverse_linked_list_rescursive.py
+++ reverse_linked_list_rescursive.py
@@ -3,9 +3,7 @@ class ListNode:
         self.val = val
         self.next = next
 
-
-
-
+# set up in itial list
 head = ListNode(1)
 head.next = ListNode(2)
 head.next.next = ListNode(3)
@@ -13,6 +11,7 @@ head.next.next.next = ListNode(4)
 head.next.next.next.next = ListNode(5)
 
 
+#print function to visualize
 def print_ll(head):
 	curr = head
 	while curr != None:
@@ -37,26 +36,15 @@ def reverseLinkedListStandard(head):
         
         return curr
 
-
-# class Solution(object):        
-#     def reverseList(self, head):  # Iterative
-#         prev, curr = None, head
-#         while curr:
-#             curr.next, prev, curr = prev, curr, curr.next
+#other solution from leetcode
+class Solution(object):        
+    def reverseList(self, head):  # Iterative
+        prev, curr = None, head
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
         
-#         return prev
+        return prev
         
-
-# public ListNode reverseList(ListNode head) {
-#     if (head == null || head.next == null) return head;
-#     ListNode p = reverseList(head.next);
-#     head.next.next = head;
-#     head.next = null;
-#     return p;
-# }
-
-
-# reverseLinkedListStandard(head)
 
 
 def reverseLinkedListRecursive(head):
@@ -70,11 +58,6 @@ def reverseLinkedListRecursive(head):
 		head.next = None;
 		return nxt
 
-# 	while current.next is not None:
-# 		current.next  = reverseLinkedListRecursive(current.next)
-
-# 	else:
-# 		return current 
 
 
 reverse = reverseLinkedListRecursive(head)
@@ -86,27 +69,3 @@ print_ll(reverse)
 	# else:
 	# 	return curr
 
-
-# def reverseLinkedListRecursive(firstNode)
-  
-#   curr = firstNode
-#   next = firstNode.next 
-
-#   if next is None:
-#     return curr
-  
-#   else:
-#   	next = reverseLinkedList(firstNode.next)
-
-
-#   current.next = prev
-#   prev = current
-#   current = nxt
-#   nxt = current.next
-
-
-# if last head:
-# 	return last head
-
-# otherwise:
-# 	point to previous and remove last
