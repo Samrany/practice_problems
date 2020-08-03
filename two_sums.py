562bf394c13b18c79554ae928d3f1790831c9c59
Shira Amrany <shira.amrany@gmail.com>
2020-08-03 20:35:19 +0000

    two pass solution)

--- two_sums.py
+++ two_sums.py
@@ -1,3 +1,4 @@
+#brute solution
 def twoSum(nums, target):
         
     length = len(nums)
@@ -8,6 +9,44 @@ def twoSum(nums, target):
                     print(idx, idx2)
                     return[idx, idx2]
 
+#two-pass hash solution
+def twoSum1(nums, target):
+    hash_table = {}
+
+    for idx in range(len(nums)):
+        hash_table[nums[idx]] = idx
+
+    for idx in range(len(nums)):
+        match = target - nums[idx]
+        if match in hash_table and hash_table[match] != idx:
+            
+            return [idx, hash_table[match]]
+
+    return []
+
+
+# hash_table={}
+# for i in range(len(nums)):    # 先做一個hash table
+#     hash_table[nums[i]]=i
+# for i in range(len(nums)):
+#     if target-nums[i] in hash_table:
+#         if hash_table[target-nums[i]] != i:
+#             return [i, hash_table[target-nums[i]]]
+# return []
+
+# public int[] twoSum(int[] nums, int target) {
+#     Map<Integer, Integer> map = new HashMap<>();
+#     for (int i = 0; i < nums.length; i++) {
+#         map.put(nums[i], i);
+#     }
+#     for (int i = 0; i < nums.length; i++) {
+#         int complement = target - nums[i];
+#         if (map.containsKey(complement) && map.get(complement) != i) {
+#             return new int[] { i, map.get(complement) };
+#         }
+#     }
+#     throw new IllegalArgumentException("No two sum solution");
+# }
 
 # def twoSum(nums: List[int], target: int) -> List[int]:
         
@@ -18,4 +57,5 @@ def twoSum(nums, target):
 #             if value + value2 == target:
 #                 return [idx, idx2]
 
-twoSum([0, 2, 5, 8, 3, 5, 2], 4)
+print(twoSum([0, 2, 5, 8, 3, 5, 2], 4))
+print(twoSum([0, 3, 6], 4))
