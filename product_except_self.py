20bf6467619033502b1f2dcee9c8244c3ce1ff26
Shira Amrany <shira.amrany@gmail.com>
2020-08-03 21:22:19 +0000

    brute approach

---
+++ product_except_self.py
@@ -0,0 +1,49 @@
+# repeat problem
+# example of input and output
+# [1, 2, 3] => [6, 3, 2]
+
+
+# ask clarifying questions
+# n will always be > 1?
+# strings or always integers?
+
+# write pseudo code
+# set empty list for output
+# length of list 
+# loop over the list:
+# 		loop over all items (val and idx) except current in list:
+# 			multiply together
+# 			append to output list
+
+# return output list
+
+
+# code
+# run through an example
+# discuss runtime/if there's a better solution
+
+def Product_except_self(nums):
+
+	output_list = []
+	length = len(nums)
+
+
+	for idx, val in enumerate(nums):
+		product = 1
+
+		for idx2, val2 in enumerate(nums):
+			if idx != idx2:
+
+				product = val2 * product
+				print(idx, idx2, val2, product)		
+		output_list.append(product)
+
+	return output_list	
+
+
+
+print(Product_except_self([1, 2, 3]) )
+print(Product_except_self([1, 2, 4]) )
+
+# 6, 3, 2
+# 8, 4, 2
