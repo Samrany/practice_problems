d05e74fa821617afc66b2b1f0f4b8e4382930afd
Shira Amrany <shira.amrany@gmail.com>
2020-08-18 03:11:09 +0000

    adjacent elements

--- century.py
+++ century.py
@@ -11,3 +11,10 @@ def checkPalindrome(inputString):
         return True
     else:
         return False
+
+def adjacentElementsProduct(inputArray):
+    output_array = []
+    for idx in range(len(inputArray)-1):
+        output_array.append(inputArray[idx]*inputArray[idx+1])
+    
+    return max(output_array)
