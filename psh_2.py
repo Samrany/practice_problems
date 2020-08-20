4bd4785756e716066de7a4ba80df7bc4e9946453
Shira Amrany <shira.amrany@gmail.com>
2020-08-20 15:02:53 +0000

    subarray

--- psh_2.py
+++ psh_2.py
@@ -1,3 +1,20 @@
 
 
 def max_subarray(nums):
+	max_sum = nums[0]
+
+	for idx, num in enumerate(nums):
+		current_sum = num
+
+		if current_sum > max_sum:
+			max_sum = current_sum
+
+		for num2 in nums[idx+1::]:
+			current_sum = current_sum + num2
+
+			if current_sum > max_sum:
+				max_sum = current_sum
+
+	return max_sum
+
+
